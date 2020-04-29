12acf3b4aa303e1edde5943d2df261fa81dfa3d1
koudai <61653253+koudai0619@users.noreply.github.com>
2020-04-29 16:48:58 +0900

    アプリの基本的なレイアウトを作成しました。

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.greenAccent[400],
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.greenAccent,
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            //containerなのはpaddingを指定することができるから
+            padding: EdgeInsets.only(
+                top: 60, left: 30, bottom: 30, right: 30), //paddingは要素の中
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  child: Icon(Icons.list,
+                      size: 30, color: Colors.lightGreenAccent),
+                  backgroundColor: Colors.white,
+                  radius: 30,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'To Do',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 tasks',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 300,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  )),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
